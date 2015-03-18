393faeb260353e673437bb6a754ed4ceeda8b317
Alex Torrance <alex.torrance@digital.cabinet-office.gov.uk>
2015-03-18 12:09:46 +0000

    Simplify SA2/3 questions

--- source/assets/javascripts/sa-2-3.js
+++ source/assets/javascripts/sa-2-3.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
   var $opts = $('.js-multi-opt');
-  console.log($opts);
   $opts.on('change', function(e) {
     var $form = $('#sa-2-3-form');
 
-    console.log($form.find('input[name=block-1]:checked').val());
-
-    if (
-      $form.find('input[name=block-1]:checked').val() == "yes" &&
-      $form.find('input[name=block-2]:checked').val() == "yes" &&
-      $form.find('input[name=block-3]:checked').val() == "yes"
-    ) {
+    if ($form.find('input[name=block-1]:checked').val() == "yes") {
       $('#sa-2-3-result').removeClass('js-hidden');
       $('#sa-2-3-button-true').removeClass('js-hidden');
       $('#sa-2-3-button-false').addClass('js-hidden');
